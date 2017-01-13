918eb04f756a5824c2ce35c71c7166e87d28c3c1
OldMan <izergind@gmail.com>
2017-01-13 06:13:43 +0300

    release

--- app/src/main/java/com/shadiz/android/weatherwatcher/model/Coord.java
+++ app/src/main/java/com/shadiz/android/weatherwatcher/model/Coord.java
@@ -1,41 +1,33 @@
 package com.shadiz.android.weatherwatcher.model;
 
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by johny homicide on 11.01.17.
  */
 
 public class Coord {
+
+    @SerializedName("lon")
     @Expose
     private Double lon;
+    @SerializedName("lat")
     @Expose
     private Double lat;
 
-    /**
-     * @return The longitude
-     */
     public Double getLon() {
         return lon;
     }
 
-    /**
-     * @param lon The longitude
-     */
     public void setLon(Double lon) {
         this.lon = lon;
     }
 
-    /**
-     * @return The latitude
-     */
     public Double getLat() {
         return lat;
     }
 
-    /**
-     * @param lat The latitude
-     */
     public void setLat(Double lat) {
         this.lat = lat;
     }
